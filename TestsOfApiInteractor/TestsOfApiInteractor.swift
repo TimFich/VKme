4177742be225daf2f6c90649aa9646511ddb0551
TimFich <timir02@yandex.ru>
2022-04-10 14:37:21 +0500

    add tests

---
+++ TestsOfApiInteractor/TestsOfApiInteractor.swift
@@ -0,0 +1,40 @@
+//
+//  TestsOfApiInteractor.swift
+//  TestsOfApiInteractor
+//
+//  Created by Тимур Миргалиев on 09.04.2022.
+//
+
+import XCTest
+@testable import VKMint
+
+let apiInteractor = APIInteractor()
+
+class TestsOfApiInteractor: XCTestCase {
+
+    override func setUpWithError() throws {
+       
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+       
+        //Given
+        
+        //When
+        
+        //Then
+        
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
